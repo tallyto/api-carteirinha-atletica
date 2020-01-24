76c6958d582cd9fe8c94e48abb40c7ebb76a0ad6
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2020-01-24 10:22:09 -0400

    Criando pagina web

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,17 @@ const socio = require('./controller/socioController');
 
 const routes = Router();
 
+routes.get('/', (req, res) => {
+  res.render('login', { title: 'Login' });
+});
+
+routes.get('/ok', (req, res) => {
+  res.render('ok', { message: 'Login efetuado com sucesso' });
+});
+routes.post('/login', (req, res) => {
+  res.redirect('/ok');
+});
+
 routes.post('/cadastrar/socio', socio.store);
 routes.get('/listar/socio', socio.index);
 
--- src/server.js
+++ src/server.js
@@ -2,13 +2,25 @@
 /* eslint-disable no-console */
 const express = require('express');
 const { connect } = require('mongoose');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const serveStatic = require('serve-static');
+const path = require('path');
 
 const server = express();
 const cors = require('cors');
 const routes = require('./routes');
 
-server.use(express.json());
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(serveStatic(path.join(__dirname, 'public')));
+
 server.use(cors());
+
+server.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+server.set('view engine', 'handlebars');
+
 
 connect(
   'mongodb+srv://otallytodev:1233211996@cluster0-jq9ag.mongodb.net/carteirinha?retryWrites=true&w=majority',
